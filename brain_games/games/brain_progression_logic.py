8ba51f7a99dd5b9566234e5d4e092a3533199356
Naumenko_Sergey <sergei.naumencko@yandex.ru>
2023-06-02 16:53:23 +0300

    refactor brain-progression

--- brain_games/games/brain_progression_logic.py
+++ brain_games/games/brain_progression_logic.py
@@ -1,5 +1,5 @@
 from brain_games.logic_for_all_games.all_logic import \
-    greeting, user_input, add_counter, arithm_progression, randint
+    greeting, user_input, add_counter, generate_arithm_progression, randint, hide_element
 
 
 def do_brain_progression_game(counter):
@@ -9,32 +9,23 @@ def do_brain_progression_game(counter):
     rools = 'What number is missing in the progression?'
     print(rools)
     while True:
-        progression = arithm_progression()
-        indx = randint(0, len(progression) - 1)
-        progression[indx] = '..'
-        question = f'Question: {progression}'
-        print(question)
-        correct_answer = progression[indx]
-        user_input_answer = user_input()
+        hidden_element, progression = hide_element(generate_arithm_progression())
+        print("Question:", " ".join(str(x) for x in progression))
+        answer = user_input()
         try:
-            user_input_answer = int(user_input_answer)
+            answer = int(answer)
         except Exception:
-            print('Incorect input. Please input inly num\'s value')
+            print('Incorect input, please input only digits')
             continue
-        progression[indx] = user_input_answer
-        if indx == len(progression) - 1:
-            correct_answer = progression[indx - 1] <= user_input_answer
-        else:
-            correct_answer = progression[indx - 1] <= user_input_answer <= progression[indx + 1]
-        if correct_answer:
-            computer_answer = 'Correct!'
+
+        if answer == hidden_element:
             counter = add_counter(counter)
+            print("Correct!")
         else:
-            answer = progression[indx + 1] - 3 if indx > 0 else progression[indx]-3
-            computer_answer = (f"'{user_input_answer}' is wrong answer ;(."
-                                f"Correct answer was '{answer}'."
+            computer_answer = (f"'{answer}' is wrong answer ;(."
+                                f"Correct answer was '{hidden_element}'."
                                 f"\nLet's try again, {usr_name}!")
-        print(computer_answer)
+            print(computer_answer)
         if counter == 3:
             print(f'Congratulations, {usr_name}!')
             break
--- brain_games/logic_for_all_games/all_logic.py
+++ brain_games/logic_for_all_games/all_logic.py
@@ -3,66 +3,70 @@ from random import randint, choice
 
 
 def greeting() -> str:
-    '''function print Hello, { inputing name } and return user_name
-    '''
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}')
-    return user_name
+	'''function print Hello, { inputing name } and return user_name
+	'''
+	user_name = prompt.string('May I have your name? ')
+	print(f'Hello, {user_name}')
+	return user_name
 
 
 def iseven(num: int) -> bool:
-    ''' This function checking is num % 2 == 0 or not
-    '''
-    flag = False
-    if num % 2 == 0:
-        flag = True
-    return flag
+	''' This function checking is num % 2 == 0 or not
+	'''
+	flag = False
+	if num % 2 == 0:
+		flag = True
+	return flag
 
 
 def validation(value):
-    if type(value) != type(int):
-        return print('Incorect input. Please input inly num\'s value')
-    else:
-        return value
+	if type(value) != type(int):
+		return print('Incorect input. Please input inly num\'s value')
+	else:
+		return value
 
 
 def user_input() -> str:
-    inp = prompt.string('Your answer: ')
-    return inp
+	inp = prompt.string('Your answer: ')
+	return inp
 
 
 def add_counter(current_counter: int) -> int:
-    current_counter += 1
-    return current_counter
+	current_counter += 1
+	return current_counter
 
 
 def generate_expression(max_value=100):
-    math_action_lst = ['+', '-', '*']
+	math_action_lst = ['+', '-', '*']
 
-    random_num1 = randint(1, max_value)
-    random_num2 = randint(1, max_value)
-    math_action = choice(math_action_lst)
+	random_num1 = randint(1, max_value)
+	random_num2 = randint(1, max_value)
+	math_action = choice(math_action_lst)
 
-    expression = f"{random_num1} {math_action} {random_num2}"
-    return expression
+	expression = f"{random_num1} {math_action} {random_num2}"
+	return expression
 
 
 def evaluate_expression(expression):
-    return eval(expression)
+	return eval(expression)
 
 
 def gcd(a: int, b: int) -> int:
-    while b:
-        a, b = b, a % b
-    return a
+	while b:
+		a, b = b, a % b
+	return a
 
 
-def arithm_progression():
-    length = randint(5, 10)
-    start = randint(1, 100)  # начальный элемент
-    diff = randint(1, 10)  # разность прогрессии
-    seq = [start]  # добавляем начальный элемент в последовательность
+def generate_arithm_progression(min_length=5, max_length=10):
+	length = randint(min_length, max_length)
+	start = randint(1, 10)
+	step = randint(1, 5)
+	progression = [start + i * step for i in range(length)]
+	return progression
 
-    for i in range(length - 1):  # заполняем последовательность до нужной длины
-        seq.append(seq[-1] + diff)
-    return seq
+
+def hide_element(progression):
+	hidden_index = randint(0, len(progression) - 1)
+	hidden_element = progression[hidden_index]
+	progression[hidden_index] = ".."
+	return hidden_element, progression
