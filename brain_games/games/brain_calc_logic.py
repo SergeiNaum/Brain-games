e542f9c16f6b96eb376f938a2a0a92d1ea518b1c
Naumenko_Sergey <sergei.naumencko@yandex.ru>
2023-06-02 10:09:07 +0300

    fix in brain_calc

--- brain_games/games/brain_calc_logic.py
+++ brain_games/games/brain_calc_logic.py
@@ -1,5 +1,5 @@
-from logic_for_all_games.all_logic import greeting,  user_input, add_counter, generate_expression, evaluate_expression, validation
-
+from logic_for_all_games.all_logic import greeting,  user_input, add_counter,\
+    generate_expression, evaluate_expression
 
 
 def do_brain_calc_game(counter):
@@ -12,17 +12,20 @@ def do_brain_calc_game(counter):
         expression = generate_expression()
         question = f'Question: {expression}'
         print(question)
+        user_input_answer = user_input()
         try:
-            user_input_answer = int(user_input())
-            correct_answer = evaluate_expression(expression)
-        except:
+            user_input_answer = int(user_input_answer)
+        except Exception:
             print('Incorect input. Please input inly num\'s value')
+            continue
+        correct_answer = evaluate_expression(expression)
         if user_input_answer == correct_answer:
             computer_answer = 'Correct!'
             counter = add_counter(counter)
         else:
-            computer_answer = (f"'{user_input_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\n"
-                               f"Let's try again, {usr_name}!")
+            computer_answer = (f"'{user_input_answer}' is wrong answer ;(."
+                               f"Correct answer was '{correct_answer}'."
+                               f"\nLet's try again, {usr_name}!")
         print(computer_answer)
         if counter == 3:
             print(f'Congratulations, {usr_name}!')
